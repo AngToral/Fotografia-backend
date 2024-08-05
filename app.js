1cd7377ab1c63964f85ed964aa1c343c48e21579
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-05 15:02:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+module.exports = { app, server };
