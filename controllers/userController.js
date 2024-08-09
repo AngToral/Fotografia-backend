263be4229bfab400af0f85212dcd24089dd23217
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-09 13:25:02 +0200

    nodemailer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { userModel } = require("../models/user.model")
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const transporter = require('../transporter')
 
 const myTokenSecret = process.env.MYTOKENSECRET //creo secreto de firma para token
 
@@ -26,7 +27,7 @@ const updateUser = async (req, res) => {
 const addUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10) //encripto contraseña
     try {
-        const user = await userModel.create({email: req.body.email, password: hashedPassword}) //creo usuario con contraseña encriptada
+        const user = await userModel.create({ email: req.body.email, password: hashedPassword }) //creo usuario con contraseña encriptada
         if (user) { return res.status(201).json({ msg: "User created" }) }
         else return res.status(404).json({ msg: "User not found" })
     } catch (error) {
@@ -35,26 +36,27 @@ const addUser = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
     try {
-        const userChecked = await userModel.findOne({email: email}) //busco email en BD
-        if (!userChecked) return res.status(404).json({msg: "This email is not registered"})
-        if(userChecked.removedAt) return res.status(404).json({msg: "Email is no longer active"})
+        const userChecked = await userModel.findOne({ email: email }) //busco email en BD
+        if (!userChecked) return res.status(404).json({ msg: "This email is not registered" })
+        if (userChecked.removedAt) return res.status(404).json({ msg: "Email is no longer active" })
         const passwordChecked = await bcrypt.compare(req.body.password, userChecked.password) // si existe email, verificamos si la contraseña es correcta
         if (passwordChecked) { //generamos token de ingreso si la contraseña es correcta
             const token = jwt.sign({ //creo token con esta info
-                id: userChecked._id, 
-                name: userChecked.name, 
-                lastname: userChecked.lastname, 
+                id: userChecked._id,
+                name: userChecked.name,
+                lastname: userChecked.lastname,
                 email: userChecked.email
             }, myTokenSecret, //doy secreto de validación
-            {expiresIn: '1h'} //expira en 1h el token
-            ) 
+                { expiresIn: '1h' } //expira en 1h el token
+            )
             console.log("token: ", token)
-            return res.status(200).json(token)}
-        return res.status(404).json({msg: "Wrong password"})
+            return res.status(200).json(token)
+        }
+        return res.status(404).json({ msg: "Wrong password" })
     } catch (error) {
-        res.status(400).json({msg: "You missed some parameter", error: error.message})
+        res.status(400).json({ msg: "You missed some parameter", error: error.message })
     }
 }
 
@@ -65,7 +67,7 @@ const verifyToken = async (req, res, next) => { //middleware que verifica token 
         req.user = decodedToken;
         next()
     } catch (error) {
-        res.status(403).json({msg: "You are not authenticated", error})
+        res.status(403).json({ msg: "You are not authenticated", error })
     }
 }
 
