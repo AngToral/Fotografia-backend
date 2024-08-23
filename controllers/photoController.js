672787ab8d9aedd1e7a102a637946828a93e01f5
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-08-23 23:16:19 +0200

    editar foto hecho

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -35,7 +35,7 @@ const updatePhoto = async (req, res) => {
             fs.unlinkSync(req.file.path);
             updateData.imageGallery = result.url;
         }
-        const photo = await photoModel.findByIdAndUpdate(req.params.id, { ...req.body })
+        const photo = await photoModel.findByIdAndUpdate(req.params.id, updateData)
         if (photo) { return res.status(200).json({ msg: "Photo updated" }) }
         else return res.status(404).json({ msg: "Photo not found" })
     } catch (error) {
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -7,7 +7,7 @@ const galleryUpload = multer({ dest: './images-gallery' })
 
 photoRouter.get('/', getPhoto)
 photoRouter.get('/:id?', getPhotoId)
-photoRouter.put('/:id?', updatePhoto)
+photoRouter.put('/:id?', galleryUpload.single('imageGallery'), updatePhoto)
 photoRouter.post('/', galleryUpload.single('imageGallery'), addPhoto)
 photoRouter.delete('/:id?', deletePhoto)
 
