6cace18e01fd645dd38517a714fa817a5dd49d0b
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-10-24 15:03:37 +0200

    volvemos

--- transporter.js
+++ transporter.js
@@ -3,10 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     //service: "Gmail",
     host: "smtp.gmail.com",
-    //port: 465,
-    port: 587,
-    //secure: true,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.GOOGLEUSER,
         pass: process.env.EMAIL_APP_PASSWORD,
